901782963651ea61eec3148b0442c3dcab6c2b42
m1ncross <andrii.bondarenko18@gmail.com>
2026-03-07 11:28:44 +0100

    ----

---
+++ Rechner-für-quad.Gleichungen.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main () {
+	var a, b, c float64
+
+	fmt.Print ("Введите первое число:")
+	fmt.Scanln(&a)
+
+	fmt.Print ("Введите второе число:")
+	fmt.Scanln($b)
+
+	fmt.Print("Введите третье число:")
+	fmt.Scanln(&c)
+
+	fmt.Print(a,b,c)
+	}
+	
